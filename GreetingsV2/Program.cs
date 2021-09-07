777f086990890d1cdecf3a9ceca460963dff41cd
Opilane TTHK <opilane@tthk.ee>
2021-09-07 14:10:08 +0300

    Add ReadLine

---
+++ GreetingsV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreetingsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
